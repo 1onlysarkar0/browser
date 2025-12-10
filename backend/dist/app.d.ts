efe6834f2ceb5ab7dfde44f15967b17314a87113
1onlysarkar0 <sauravmiami@gmail.com>
2025-12-10 17:33:37 +0530

    Delete backend directory

--- backend/dist/app.d.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from 'socket.io';
-declare const app: import("express-serve-static-core").Express;
-declare const io: Server<import("socket.io").DefaultEventsMap, import("socket.io").DefaultEventsMap, import("socket.io").DefaultEventsMap, any>;
-export declare const emitUrlUpdate: (urlId: string, data: any) => void;
-export declare const emitExecutionStart: (urlId: string, data: any) => void;
-export declare const emitExecutionComplete: (urlId: string, data: any) => void;
-export declare const emitScreenshotCaptured: (urlId: string, data: any) => void;
-export { app, io };
-//# sourceMappingURL=app.d.ts.map
